196a83dcee229220ff30e37936f0fda342f79f04
tonygalmiche <tony.galmiche@infosaone.com>
2013-11-18 14:40:30 +0100

    add static/src/img/icon.png

---
+++ __openerp__.py
@@ -0,0 +1,34 @@
+# -*- coding: utf-8 -*-
+
+# Doc : https://doc.openerp.com/trunk/server/03_module_dev_01/
+#       https://doc.openerp.com/trunk/server/03_module_dev_01/#manifest-file-openerp-py
+
+{
+  "name" : "InfoSaône - Module OpenERP Tutoriel 02",
+  "version" : "0.1",
+  "author" : "InfoSaône",
+  "category" : "InfoSaône/Tutoriel",
+
+
+  'description': """
+InfoSaône / Module OpenERP Tutoriel 02 
+===================================================
+Ce module n'a aucune fonctionnalité. 
+Son seul but est de montrer comment créer et installer un premier module dans OpenERP
+
+""",
+
+  'maintainer': 'InfoSaône',
+  'website': 'http://www.infosaone.com',
+
+  "depends" : [],
+  "init_xml" : [],
+  "demo_xml" : [],
+  "update_xml" : [],
+  "installable": True,
+  "active": False
+}
+
+
+
+
